36eff9ae7c76bc02ea3edbfcfb917ef83d56c1c9
Nirvana2512 <63945015+Nirvana2512@users.noreply.github.com>
2021-05-28 07:22:57 +0530

    Add files via upload

---
+++ web-worker.js
@@ -0,0 +1,19 @@
+let data;
+self.onmessage = (e)=>{
+    
+   data = e.data;
+    var numberData = Number(data);
+
+    for(let i =0;i<numberData;i++)
+    {
+
+        if(i%4===0)
+        {
+            self.postMessage(i);
+        }
+        
+
+    }
+    self.postMessage("DONE");
+
+}
